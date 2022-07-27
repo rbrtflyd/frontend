bb04da5031f53a0613d66c4a5d82d0b6d2bb9923
Robert Floyd <rbrt@hey.com>
2022-07-26 23:28:16 -0400

    Hook up tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,14 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
-}
+  siteName: "Gridsome",
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss2",
+      options: {
+        tailwindConfigFile: "./tailwind.config.js",
+        mainCssFile: "./src/main.css",
+      },
+    },
+  ],
+};
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,6 @@
 <template>
   <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
+    <h1 class="text-lg text-orange-400">Hello, world!</h1>
 
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
@@ -22,7 +19,6 @@
         >GitHub</a
       >
     </p>
-    cgange
   </Layout>
 </template>
 
